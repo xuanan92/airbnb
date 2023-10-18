b5ce16cd95727533929f72c3ac020f80927ce3f7
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-10-19 04:29:21 +0700

    update baseUrl

--- client/src/api/apiSlice.js
+++ client/src/api/apiSlice.js
@@ -1,6 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "https://localhost:5005/";
+const baseUrl_local = "https://localhost:5005/";
+const baseUrl_public = "https://103.216.113.219:5005/";
+let baseUrl = "";
+if (process.env.NODE_ENV === "production") {
+  baseUrl = baseUrl_public;
+} else {
+  baseUrl = baseUrl_local;
+}
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl }),
