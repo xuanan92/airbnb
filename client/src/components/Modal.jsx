e609d1fadc7d81d35eb1f232a1320097f92401c8
xuanan92 <nguyentruongxuananhn@gmail.com>
2023-11-06 12:09:39 +0700

    fix order link li in modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const Modal = () => {
   return (
     <>
@@ -5,14 +6,24 @@ const Modal = () => {
         className="fixed inset-0 z-40 bg-transparent"
         id="modalWrapper"
       ></div>
-      <div className="absolute py-1 w-60 text-black z-50 -bottom-1 right-0 translate-y-[100%] bg-white rounded-lg border shadow-md text-left">
+      <div className="absolute  py-1 w-60 text-black z-50 -bottom-1 right-0 translate-y-[100%] bg-white rounded-lg border shadow-md text-left">
         <ul className="flex flex-col">
-          <li className="p-4 font-bold hover:bg-gray-100">Sign up</li>
-          <li className="p-4 hover:bg-gray-100">Log in</li>
+          <Link to="" className="p-4 font-bold hover:bg-gray-100">
+            <li>Sign up</li>
+          </Link>
+          <Link className="p-4 hover:bg-gray-100">
+            <li>Log in</li>
+          </Link>
           <hr />
-          <li className="p-4 hover:bg-gray-100">Gift cards</li>
-          <li className="p-4 hover:bg-gray-100">Airbnb your home</li>
-          <li className="p-4 hover:bg-gray-100">Help Center</li>
+          <Link className="p-4 hover:bg-gray-100">
+            <li>Gift cards</li>
+          </Link>
+          <Link className="p-4 hover:bg-gray-100">
+            <li>Airbnb your home</li>
+          </Link>
+          <Link className="p-4 hover:bg-gray-100">
+            <li>Help Center</li>
+          </Link>
         </ul>
       </div>
     </>
